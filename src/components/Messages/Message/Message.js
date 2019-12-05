319e4b9abb2cca8d8cb1d3725637f34b68a2ccef
Beksultan Kadyrbekov <kbeksss@gmail.com>
2019-12-06 03:18:00 +0600

    edited component that shows all last messages

--- src/components/Messages/Message/Message.js
+++ src/components/Messages/Message/Message.js
@@ -1,10 +1,13 @@
 import React, {Component} from 'react';
+import './Message.css';
 
 class Message extends Component {
     render() {
         return (
             <div className='Message'>
-
+                <div className='MessageWrapper Author'><div>author</div>Beks</div>
+                <div className='MessageWrapper Text'><div>text</div>Hello</div>
+                <div className='MessageWrapper Date'><div>date</div>11.12.2019</div>
             </div>
         );
     }
