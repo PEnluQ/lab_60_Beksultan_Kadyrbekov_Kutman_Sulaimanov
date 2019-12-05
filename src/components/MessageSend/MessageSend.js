e77154edf4f1b97ddf69901fcc807fd1f6e0b1b5
Beksultan Kadyrbekov <kbeksss@gmail.com>
2019-12-06 03:32:17 +0600

    enabled two-way binding to inputs in the form

--- src/components/MessageSend/MessageSend.js
+++ src/components/MessageSend/MessageSend.js
@@ -5,9 +5,9 @@ const MessageSend = (props) => {
     return (
         <div className='MessageSend'>
             <form action="#" onSubmit={props.Submit}>
-                <div className='SendWrapper'><span>author </span><input type="text"/></div>
-                <div className='SendWrapper'><span>message </span><input type="text"/></div>
-                <div className='SendWrapper'><button>Send</button></div>
+                <div className='SendWrapper'><span>author </span><input type="text" onChange={props.authorChange} value={props.authorValue}/></div>
+                <div className='SendWrapper'><span>message </span><input type="text" onChange={props.textChange} value={props.textValue}/></div>
+                <div className='SendWrapper'><button type='submit'>Send</button></div>
             </form>
         </div>
     );
--- src/container/Messenger/Messenger.js
+++ src/container/Messenger/Messenger.js
@@ -3,11 +3,35 @@ import MessageSend from "../../components/MessageSend/MessageSend";
 import Messages from "../../components/Messages/Messages";
 
 class Messenger extends Component {
+    state = {
+        currentMessage: {author: 'Beks', message: 'Hello, guys'},
+    };
+    submitMessage = (event) => {
+        event.preventDefault();
+        console.log('hello');
+    };
+    changeAuthor = (event) => {
+        const currentMessage = {...this.state.currentMessage};
+        currentMessage.author = event.target.value;
+        console.log(currentMessage);
+        this.setState({currentMessage}); //todo: have to check if it is correct way
+    };
+    changeText = (event) => {
+        const currentMessage = {...this.state.currentMessage};
+        currentMessage.message = event.target.value;
+        this.setState({currentMessage}) //todo: have to check if it is correct way
+    };
     render() {
         console.log('hello');
         return (
             <div className='Messenger'>
-                <MessageSend/>
+                <MessageSend
+                    Submit={(event) => this.submitMessage(event)}
+                    authorValue={this.state.currentMessage.author}
+                    textValue={this.state.currentMessage.message}
+                    authorChange={(event) => this.changeAuthor(event)}
+                    textChange={(event) => this.changeText(event)}
+                />
                 <Messages/>
             </div>
         );
