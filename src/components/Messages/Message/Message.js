39649286d215480686446c1a7cc6b92b5c16df4e
Beksultan Kadyrbekov <kbeksss@gmail.com>
2019-12-06 16:05:50 +0600

    Changed the date format

--- src/components/Messages/Message/Message.js
+++ src/components/Messages/Message/Message.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import './Message.css';
-
+import moment from 'moment';
 class Message extends Component {
     shouldComponentUpdate(nextProps, nextState, nextContext) {
         return nextProps.id !== this.props.id
@@ -12,7 +12,7 @@ class Message extends Component {
             <div className='Message'>
                 <div className='MessageWrapper Author'><div>author</div>{this.props.author}</div>
                 <div className='MessageWrapper Text'><div>text</div>{this.props.text}</div>
-                <div className='MessageWrapper Date'><div>date</div>{this.props.date}</div>
+                <div className='MessageWrapper Date'><div>date</div>{moment(this.props.date).format('LLL')}</div>
             </div>
         );
     }
